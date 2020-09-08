e7f8af366bab22a9df3ff1b9a22cd1f852937345
Camilo Sasuke Tsumanuma <camilo@ca2.email>
2020-09-08 13:32:34 -0300

    <3tbs, Mummi and bilbo!!
    jitspoe and strager streaming contribution!!
    
    jhoovgaard streaming (Big Ambitions game development) contribution!!
    
    GexYT streaming contribution!!
    
    Ash_F0x streaming contribution!!
    
    Jaxstyle streaming contribution!!
    
    cupsarestupid chatting contribution!!
    
    Trainwreckstv & Friend streaming contribution!!
    
    ShemicalX and Friend streaming contribution!!
    
    BarRaider streaming contribution!!
    
    unicorn_dev (& xmetrix) streaming contribution!!
    
    TheCherno streaming contribution!!
    
    rw_grin and FearItSelf(&Friends) streaming contribution!!
    
    Naysayer88 and friends contribution!!
    
    lelandkwong streaming contribution!!
    
    TimBeaudet and FletcherLabs streaming contribution!!
    
    oijtx streaming contribution!!
    
    arn4vv and parrot_rl chatting contribution!!
    
    lowluds chatting and interaction contribution!!
    
    NorthSeaHero and FearItSelf streaming contribution!!
    
    AdamCYounis and BarRaider streaming contribution!!
    
    sodapoppin and friends contribution!!
    
    Greekgodx & FletcherLas streaming contribution (at September: His month ? (a party ?) at least for me, just Don't wake me up for nothing!!) (Saturday Dawn / Morning)!!
    
    lelandkwong streaming contribution!!
    
    oijtx streaming contribution!!
    
    -----
    eddie machine running Windows 10 remotely from bilbo running Windows 10

--- helloearth/main.cpp
+++ helloearth/main.cpp
@@ -1,22 +1,41 @@
-#define NO_DRAW2D
-#define NO_IMAGING
-#include "aura/console_slim.h"
+#include "acme/console_slim.h"
 
 
 int main(int argc, wchar_t * argv[])
 {
 
-   //console console(argc, argv);
-
    string strHelloEarth = "Hello Earth!!";
 
    printf("%s\n", strHelloEarth.c_str());
 
+   string strTest123;
+
+   strTest123 = "Test123";
+
+   printf("Test123=%s\n", strTest123.c_str());
+
+   ::str::begins_eat_ci(strTest123, "test");
+
+   printf("begins_eat_ci(strTest123, \"test\")=%s\n", strTest123.c_str());
+
+   string_array stra;
+
+   stra.add("apple");
+   stra.add("orange");
+   stra.add("strawberry");
+
+   for (auto& str : stra)
+   {
+
+      printf("a fruit: %s\n", str.c_str());
+
+   }
+
+
+
    press_any_key_to_exit();
 
    return 0;
-
-   //return console.result();
 
 }
 
