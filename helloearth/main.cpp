f71045c6df028b14c00ace37c44c410dab9f177d
Camilo Sasuke Tsumanuma <camilo@ca2.email>
2020-09-14 11:49:52 -0300

    🌌 🌍 [ ca2 ] [ C++ ] Splitting ca2 more : acme/apex - aura/axis/base/core
    <3tbs, Mummi and bilbo!!
    
    Twitch.tv streamers contribution!!
    
    Twitch.tv chatters contribution!!
    
    vue_equalizer (fpgaguru) chatting contribution!!
    
    Kebabskal, jhovgaard and DonHo streaming contribution!!
    
    sodapoppin and friends contribution!!
    
    Naysayer88 and friends contribution!!
    
    lelandkwong streaming contribution!!
    
    jhoovgaard, BendixBoy and Ash_F0x streaming contribution!!
    
    JessicaMak streaming contribution!!
    
    Trainwreckstv streaming contribution!!
    
    oijtx streaming contribution!!
    
    jitspoe and strager streaming contribution!!
    
    Trainwreckstv & Friend streaming contribution!!
    
    ShemicalX and Friend streaming contribution!!
    
    BarRaider streaming contribution!!
    
    unicorn_dev (& xmetrix) streaming contribution!!
    
    TheCherno streaming contribution!!
    
    rw_grin and FearItSelf(&Friends) streaming contribution!!
    
    Naysayer88 and friends contribution!!
    
    lelandkwong streaming contribution!!
    
    TimBeaudet and FletcherLabs streaming contribution!!
    
    oijtx streaming contribution!!
    
    arn4vv and parrot_rl chatting contribution!!
    
    lowluds chatting and interaction contribution!!
    
    NorthSeaHero and FearItSelf streaming contribution!!
    
    AdamCYounis and BarRaider streaming contribution!!
    
    sodapoppin and friends contribution!!
    
    Greekgodx & FletcherLas streaming contribution (at September: His month ? (a party ?) at least for me, just Don't wake me up for nothing!!) (Saturday Dawn / Morning)!!
    
    lelandkwong streaming contribution!!
    
    oijtx streaming contribution!!
    
    jitspoe and strager streaming contribution!!
    
    jhoovgaard streaming (Big Ambitions game development) contribution!!
    
    GexYT streaming contribution!!
    
    Ash_F0x streaming contribution!!
    
    Jaxstyle streaming contribution!!
    
    cupsarestupid chatting contribution!!
    
    Trainwreckstv & Friend streaming contribution!!
    
    ShemicalX and Friend streaming contribution!!
    
    BarRaider streaming contribution!!
    
    unicorn_dev (& xmetrix) streaming contribution!!
    
    TheCherno streaming contribution!!
    
    rw_grin and FearItSelf(&Friends) streaming contribution!!
    
    Naysayer88 and friends contribution!!
    
    lelandkwong streaming contribution!!
    
    TimBeaudet and FletcherLabs streaming contribution!!
    
    oijtx streaming contribution!!
    
    arn4vv and parrot_rl chatting contribution!!
    
    lowluds chatting and interaction contribution!!
    
    NorthSeaHero and FearItSelf streaming contribution!!
    
    AdamCYounis and BarRaider streaming contribution!!
    
    sodapoppin and friends contribution!!
    
    Greekgodx & FletcherLas streaming contribution (at September: His month ? (a party ?) at least for me, just Don't wake me up for nothing!!) (Saturday Dawn / Morning)!!
    
    lelandkwong streaming contribution!!
    
    oijtx streaming contribution!!
    
    -----
    eddie machine running Windows 10 remotely from bilbo running Windows 10

--- helloearth/main.cpp
+++ helloearth/main.cpp
@@ -1,4 +1,4 @@
-#include "acme/console_slim.h"
+#include "acme/console.h"
 
 
 int main(int argc, wchar_t * argv[])
@@ -31,7 +31,56 @@ int main(int argc, wchar_t * argv[])
 
    }
 
+   ::id id;
 
+   id = 1;
+
+   printf("id=%s\n", id.to_string().c_str());
+
+   id = "two";
+
+   printf("id=%s\n", id.to_string().c_str());
+
+   property_set set;
+
+   auto & library = set["user1"];
+
+   library["book1"]["title"] = "First C++ Book";
+   library["book1"]["chapter1"] = "First C++ Book - First Chapter";
+   library["book1"]["chapter2"] = "First C++ Book - Second Chapter";
+   library["book1"]["chapter3"] = "First C++ Book - Last Chapter";
+   library["book1"]["page_count"] = 40;
+   library["book1"]["first_edition_weight"] = 0.621;
+   library["book2"]["title"] = "Second C++ Book";
+   library["book2"]["chapter1"] = "First C++ Book - First Chapter";
+   library["book2"]["chapter2"] = "First C++ Book - Second Chapter";
+   library["book2"]["chapter3"] = "First C++ Book - Third Chapter";
+   library["book2"]["chapter4"] = "First C++ Book - Fourth Chapter";
+   library["book2"]["chapter5"] = "First C++ Book - Last Chapter";
+   library["book2"]["page_count"] = 81;
+   library["book2"]["first_edition_weight"] = 0.907;
+
+
+   auto& library2 = set["user2"];
+
+   library2["book1"]["title"] = "First C Book";
+   library2["book1"]["chapter1"] = "First C++ Book - First Chapter";
+   library2["book1"]["chapter2"] = "First C++ Book - Last Chapter";
+   library2["book1"]["page_count"] = 32;
+   library2["book1"]["first_edition_weight"] = 0.517;
+
+   library2["book2"]["title"] = "Second C Book";
+   library2["book2"]["chapter1"] = "First C++ Book - First Chapter";
+   library2["book2"]["chapter2"] = "First C++ Book - Second Chapter";
+   library2["book2"]["chapter3"] = "First C++ Book - Third Chapter";
+   library2["book2"]["chapter4"] = "First C++ Book - Last Chapter";
+   library2["book2"]["page_count"] = 72;
+   library2["book2"]["editions_weight"] = var_array({ 0.907, 0.856, 0.741 });
+
+
+   string strJson = set.get_json();
+
+   printf("%s\n", strJson.c_str());
 
    press_any_key_to_exit();
 
